3c672ad7c5e111345ba7d8609751a5c8c8dabce9
André Rodrigues <andre88.rg@gmail.com>
2021-03-23 17:48:31 -0300

    aplicando refactoring na classe ReajusteService.

--- src/main/java/br/com/alura/tdd/service/ReajusteService.java
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -8,20 +8,7 @@ import java.math.BigDecimal;
 public class ReajusteService {
 
     public void concederReajuste(Funcionario funcionario, Desempenho desempenho) {
-        BigDecimal reajuste = BigDecimal.ZERO;
-        switch (desempenho) {
-            case A_DESEJAR:
-                reajuste = funcionario.getSalario().multiply(new BigDecimal("0.03"));
-                break;
-            case BOM:
-                reajuste = funcionario.getSalario().multiply(new BigDecimal("0.15"));
-                break;
-            case OTIMO:
-                reajuste = funcionario.getSalario().multiply(new BigDecimal("0.20"));
-                break;
-            default:
-                break;
-        }
-        funcionario.reajustarSalario(reajuste);
+        BigDecimal percentual = desempenho.percentualReajuste();
+        funcionario.reajustarSalario(funcionario.getSalario().multiply(percentual));
     }
 }
