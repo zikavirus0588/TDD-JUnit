18a92719e895b4097a6b06d526dc3fcfdb8a954f
André Rodrigues <andre88.rg@gmail.com>
2021-03-23 19:35:22 -0300

    lançando e testando exception no bonus service.

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -7,15 +7,17 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BonusServiceTest {
 
     @Test
-    public void bonusDeveriaSerZeroParaFuncionarioComSalarioMaiorQue10000() {
+    public void bonusDeveriaLancarExceptionParaFuncionarioComSalarioMaiorQue10000() {
         BonusService service = new BonusService();
-        BigDecimal bonus = service.calcularBonus(new Funcionario("Zica", LocalDate.now(),new BigDecimal("12000")));
+        assertThrows(IllegalArgumentException.class,
+                () -> service.calcularBonus(new Funcionario("Zica", LocalDate.now(),new BigDecimal("12000"))),
+                "Funcionário com salário maior que R$ 10.000,00 não recebe bônus.");
 
-        assertEquals(new BigDecimal("0.00"),bonus);
     }
     @Test
     public void bonusDeveriaSer10PorCentoParaSalarioExatamenteIgualA1000() {
